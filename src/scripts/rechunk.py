85fbe9dad5f4936a8d5b63dd282b1ac493b9f4e3
jmoore <josh@glencoesoftware.com>
2020-04-29 14:35:49 +0200

    rechunk: add distributed flag with help

--- src/scripts/rechunk.py
+++ src/scripts/rechunk.py
@@ -37,11 +37,23 @@ def convert(resized, target_array):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dimensions", type=int, default="5")
-    parser.add_argument("source_array")
-    parser.add_argument("target_array")
-    parser.add_argument("chunks")
+    parser.add_argument("--dimensions", type=int, default="5", metavar="DIMS",
+                        help="number of chunks to parse (%(default)s)")
+    parser.add_argument("--distributed", action="store_true",
+                        help="enable distributed dashboard (%(default)s)")
+    parser.add_argument("source_array",
+                        help="array to copy from. must exist.")
+    parser.add_argument("target_array",
+                        help="array to write to. may not exist.")
+    parser.add_argument("chunks", default="1024,1024,1,1,1",
+                        help=("comma-separated string of chunk sizes "
+                              "(%(default)s)"))
     args = parser.parse_args()
+
+    if args.distributed:
+        from dask.distributed import Client
+        client = Client()
+        input("Visit http://localhost:8787/status - Press Enter to continue...")
 
     chunks = [int(x) for x in args.chunks.split(",")]
     assert len(chunks) == args.dimensions
@@ -53,6 +65,9 @@ def main():
         ),
         args.target_array
     )
+
+    if args.distributed:
+        input("Press Enter to exit...")
 
 
 if __name__ == "__main__":
