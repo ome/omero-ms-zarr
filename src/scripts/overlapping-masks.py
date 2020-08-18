994b59ca0beccab7645ddc5904316c9535a2a707
jmoore <josh@glencoesoftware.com>
2020-08-18 07:23:02 +0200

    Update comment in script

--- src/scripts/overlapping-masks.py
+++ src/scripts/overlapping-masks.py
@@ -17,7 +17,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-# Copy masks from one server to another.
+# Generate overlapping masks on an existing image.
 # author: m.t.b.carroll@dundee.ac.uk
 
 from omero.gateway import BlitzGateway
